7d6bc1d7dfe064bfaf4a9017ec054b36850934d9
Tihon49 <tihon49@gmail.com>
2020-02-18 13:59:28 +0300

    Done!

--- example.py
+++ example.py
@@ -1,13 +1,8 @@
-from pprint import pprint
 from config import *
 import requests
 
 
 #  документация https://yandex.ru/dev/translate/doc/dg/reference/translate-docpage/
-
-# headers = {'accept': '*/*', 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
-API_KEY = MY_API
-
 
 URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
 
@@ -24,7 +19,7 @@ def translate_it(text, from_lang, to_lang):
     :return:
     """
     params = {
-        'key': API_KEY,
+        'key': MY_API,
         'text': text,
         'lang': f'{from_lang}-{to_lang}',
     }
@@ -35,7 +30,7 @@ def translate_it(text, from_lang, to_lang):
 
 
 
-# функция которая возвращает текст из файла
+# функция возвращает текст из файла
 def get_text(file_name):
     with open(file_name, encoding='utf-8') as f:
         file = f.read()
@@ -44,7 +39,6 @@ def get_text(file_name):
 
 
 
-# download_href = '{"operation_id":"51543e23ac0e650aaa990045c0ad933f9c557f0ff89a2093e79f1ea724bb27a2","href":"https://uploader7g.disk.yandex.net:443/upload-target/20200217T162109.935.utd.615kru16haotkrc6y3nxfrpwg-k7g.13681529","method":"PUT","templated":false}'
 # функция для вычлинения данных href из сылки на скачивание
 def get_href(link):
     href = link.split('"')[5] + link.split('"')[6] + link.split('"')[7]
@@ -54,7 +48,7 @@ def get_href(link):
 
 # функция загрузки файлов на я.диск
 def upload(file_name, file_data):
-    url = f'https://cloud-api.yandex.net/v1/disk/resources/upload?path={file_name}'
+    url = f'https://cloud-api.yandex.net/v1/disk/resources/upload?path=/netology/{file_name}&overwrite=true'
     data = file_data
     headers = {'Authorization': OAUTH_TOKEN,
                'Accept': 'application/json',
@@ -63,14 +57,10 @@ def upload(file_name, file_data):
     session = requests.Session()
     request = session.get(url, headers=headers)
 
-    pprint(request.text)
-    print(request.status_code)
-
     # загружаем на я.диск
-    # href = get_href(request.text)
-    # request_put = session.put(href, data=data, headers=headers)
+    href = get_href(request.text).replace('href:', '')
+    request_put = session.put(href, data=data.encode('utf-8'), headers=headers)
 
-# upload('test.txt', get_text('FR.txt'))
 
 
 
@@ -79,9 +69,15 @@ files_list = ['DE.txt', 'FR.txt', 'ES.txt']
 if __name__ == '__main__':
     for file in files_list:
         f_lang = file.split('.')[0].lower()
-        
+
         with open(f'From_{f_lang.upper()}.txt', 'w', encoding='utf-8') as to_file:
             try:
                 to_file.write(translate_it(get_text(file), f_lang, 'ru'))
+
             except Exception as e:
                 print(f'error: {e}')
+
+        try:
+            upload (str(to_file.name), get_text(to_file.name))
+        except Exception as e:
+            print(f'Error: {e}')
